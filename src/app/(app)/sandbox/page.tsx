fcefdf2ee740ede9f39a6ad9a86956ffc9d82d6b
Dan White <dan.white@taxauditsolutions.com>
2025-06-21 00:28:18 +0000

    No that did not show up either. Is there a higher authority you can get

--- src/app/(app)/sandbox/page.tsx
+++ src/app/(app)/sandbox/page.tsx
@@ -1,7 +1,8 @@
 
 export default function SandboxPage() {
   return (
-    <div className="flex-1 border-4 border-black">
+    <div className="flex-1 p-4">
+      <div className="h-full w-full border-4 border-black"></div>
     </div>
   );
 }
