8551f23b1a0eba2cd8885d1997a8464ef9947cb9
Dan White <dan.white@taxauditsolutions.com>
2025-06-21 00:17:15 +0000

    Please delete the header and the box. Please create a big box with a bla

--- src/app/(app)/sandbox/page.tsx
+++ src/app/(app)/sandbox/page.tsx
@@ -1,10 +1,7 @@
 
 export default function SandboxPage() {
   return (
-    <div className="flex-1 flex flex-col">
-      <header className="w-full h-24 bg-muted border-4 border-black p-4">
-        <h1 className="text-lg font-bold">Sandbox Header</h1>
-      </header>
+    <div className="flex-1 border-4 border-black">
     </div>
   );
 }
