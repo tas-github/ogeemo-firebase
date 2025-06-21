9288b9758d2d195130a1702344301db92c892f28
Dan White <dan.white@taxauditsolutions.com>
2025-06-21 00:30:35 +0000

    Nope, all we have is one black box where the side bar is located. Nothin

--- src/app/(app)/sandbox/page.tsx
+++ src/app/(app)/sandbox/page.tsx
@@ -1,8 +1,6 @@
 
 export default function SandboxPage() {
   return (
-    <div className="flex-1 p-4">
-      <div className="h-full w-full border-4 border-black"></div>
-    </div>
+    <div className="h-full w-full border-4 border-black"></div>
   );
 }
