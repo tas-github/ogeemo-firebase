3eeebf0a575ca75cab45c411a426f8c40532d14d
Dan White <dan.white@taxauditsolutions.com>
2025-06-22 23:51:53 +0000

    The General Tasks button should have the name changed to "Project List"

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,6 +6,6 @@ export interface Project {
 }
 
 export const initialProjects: Project[] = [
-  { id: 'proj-1', name: 'General Tasks', description: 'A collection of miscellaneous tasks and to-dos.' },
+  { id: 'proj-1', name: 'Project List', description: 'A collection of miscellaneous tasks and to-dos.' },
   { id: 'proj-2', name: 'Website Redesign V2', description: 'Complete overhaul of the company website, including UI/UX and backend integration.' },
 ];
