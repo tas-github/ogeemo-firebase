84658971c9cd2005214ddbf001db137647c0b51c
Dan White <dan.white@taxauditsolutions.com>
2025-06-22 15:02:38 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type {Config} from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -95,4 +95,6 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
-} satisfies Config;
+};
+
+export default config;
