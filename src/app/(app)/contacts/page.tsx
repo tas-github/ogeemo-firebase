b00d9d94cbdb3cbd8fc671cddc0dfaa3b41e680f
Nick Ilio <nick@taxauditsolutions.com>
2025-06-22 19:44:48 +0000

    On the Ogeemo Contact Manager page you have a button called Test Box tha

--- src/app/(app)/contacts/page.tsx
+++ src/app/(app)/contacts/page.tsx
@@ -482,7 +482,7 @@ export default function ContactsPage() {
       </Dialog>
       
       <Dialog open={isTestDialogOpen} onOpenChange={setIsTestDialogOpen}>
-        <DialogContent className="w-[95vw] max-w-4xl h-[90vh] flex flex-col p-0">
+        <DialogContent className="w-full h-full max-w-none top-0 left-0 translate-x-0 translate-y-0 rounded-none sm:rounded-none flex flex-col p-0">
           <DialogHeader className="p-6 pb-4 border-b">
             <DialogTitle>Test Dialog Box</DialogTitle>
             <DialogDescription>
