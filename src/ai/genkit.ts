61722447bbb122225c46df15b2177b13e85b284d
Nick Ilio <nick@taxauditsolutions.com>
2025-06-23 13:55:33 +0000

    This is not working. You need to find the root problem. Remember that th

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,19 +2,7 @@ import {genkit, type GenkitConfig} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 const genkitConfig: GenkitConfig = {
-  plugins: [],
+  plugins: [googleAI()],
 };
-
-export const aiFeaturesEnabled = !!process.env.GOOGLE_API_KEY;
-
-if (aiFeaturesEnabled) {
-  genkitConfig.plugins?.push(googleAI());
-  genkitConfig.model = 'googleai/gemini-1.5-flash-latest';
-  
-} else {
-  console.warn(
-    'WARNING: GOOGLE_API_KEY environment variable is not set. Genkit AI features will be disabled.'
-  );
-}
 
 export const ai = genkit(genkitConfig);
