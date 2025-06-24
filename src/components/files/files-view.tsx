e77b0aa62cdff35d642c2b91d15d913baca3c4a5
Dan White <dan.white@taxauditsolutions.com>
2025-06-24 14:14:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/files/files-view.tsx
+++ src/components/files/files-view.tsx
@@ -46,6 +46,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -247,7 +248,7 @@ export function FilesView() {
         title: "Upload Successful",
         description: `${newFiles.length} file(s) have been added to "${targetFolder?.name}".`
       });
-      e.target.value = '';
+      e.target.value = ''; // Reset file input
 
       setIsUploadDialogOpen(false);
       setSelectedFolderId(uploadTargetFolderId);
