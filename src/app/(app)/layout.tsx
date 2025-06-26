15392d8de9f947d5e965ebd7f7b9733c3a560ff1
Nick Ilio <nick@taxauditsolutions.com>
2025-06-26 04:33:21 +0000

    yes, please proceed

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,13 +1,16 @@
 
 import { ClientLayout } from "@/components/layout/client-layout";
 import { AuthProvider } from "@/context/auth-context";
+import { NavigationProvider } from "@/context/navigation-context";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <ClientLayout>
-        {children}
-      </ClientLayout>
+      <NavigationProvider>
+        <ClientLayout>
+          {children}
+        </ClientLayout>
+      </NavigationProvider>
     </AuthProvider>
   );
 }
