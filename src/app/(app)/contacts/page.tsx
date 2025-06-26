5ea512d4487286ae9afac0332dd58dfd3a3e7e78
Nick Ilio <nick@taxauditsolutions.com>
2025-06-26 03:26:31 +0000

    ok, I'm curious to see what the Skeleton loader will look like, but I wo

--- src/app/(app)/contacts/page.tsx
+++ src/app/(app)/contacts/page.tsx
@@ -1,19 +1,12 @@
 
 import dynamic from 'next/dynamic';
-import { LoaderCircle } from 'lucide-react';
+import { ContactsSkeleton } from '@/components/contacts/contacts-skeleton';
 
 const ContactsView = dynamic(
   () => import('@/components/contacts/contacts-view').then((mod) => mod.ContactsView),
   {
     ssr: false,
-    loading: () => (
-      <div className="flex h-full w-full items-center justify-center p-4">
-        <div className="flex flex-col items-center gap-4">
-          <LoaderCircle className="h-10 w-10 animate-spin text-primary" />
-          <p className="text-muted-foreground">Loading Contacts...</p>
-        </div>
-      </div>
-    ),
+    loading: () => <ContactsSkeleton />,
   }
 );
 
