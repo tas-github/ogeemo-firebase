c33d9ba28e780ec60190cd6b132324f4d6f7f486
Dan White <dan.white@taxauditsolutions.com>
2025-06-27 13:05:51 +0000

    Centre justify the header called;  Create a new task, dialog box

--- src/components/tasks/NewTaskDialog.tsx
+++ src/components/tasks/NewTaskDialog.tsx
@@ -266,7 +266,7 @@ export function NewTaskDialog({ isOpen, onOpenChange, defaultStartDate, eventToE
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent className="w-full h-full max-w-none top-0 left-0 translate-x-0 translate-y-0 rounded-none sm:rounded-none flex flex-col p-0">
-        <DialogHeader className="p-6 pb-4 border-b text-center">
+        <DialogHeader className="p-6 pb-4 border-b text-center sm:text-center">
           <DialogTitle className="text-2xl font-bold font-headline text-primary">{isEditMode ? "Edit Task" : "Create a New Task"}</DialogTitle>
           <DialogDescription>
             {isEditMode ? "Update the details for your task." : "Fill out the details below to add a new task to your board."}
