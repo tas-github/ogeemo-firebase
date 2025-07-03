42d98973b9434a821c0eabe9060ab87cbed50926
Dan White <dan.white@taxauditsolutions.com>
2025-07-03 14:17:32 +0000

    Interesting it is now showing images that are unrelated to the specific

--- next.config.ts
+++ next.config.ts
@@ -17,12 +17,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
