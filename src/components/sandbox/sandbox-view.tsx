8be9ecc89a0aa2481440ce93a8a9714491d0e874
Dan White <dan.white@taxauditsolutions.com>
2025-07-04 14:56:52 +0000

    That did not work, so I suggest to go back to the sandbox. add a button

--- src/components/sandbox/sandbox-view.tsx
+++ src/components/sandbox/sandbox-view.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { TransactionsView } from "./transactions-view";
+import { Button } from "@/components/ui/button";
 
 export function SandboxView() {
-  return <TransactionsView />;
+  return (
+    <div className="p-4 sm:p-6 flex items-center justify-center h-full">
+      <Button>Transaction Hub</Button>
+    </div>
+  );
 }
