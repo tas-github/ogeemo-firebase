2e4646ee8258d565f19bb644f4b8008d3276020a
Dan White <dan.white@taxauditsolutions.com>
2025-07-08 11:44:27 +0000

    I got this message; studio.firebase.google.com refused to connect.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <Link href="https://studio.firebase.google.com/studio-6489696380">
+    <Link href="https://studio.firebase.google.com/studio-6489696380" target="_top">
       <div className={cn("flex items-center gap-2 cursor-pointer", className)} {...props}>
         <Image
           src="/images/Ogeemo-Logo-BonT.png"
