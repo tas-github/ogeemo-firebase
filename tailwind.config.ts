9ee35f96a83bbb8cc9200d4ad6aa7ba3c6231df0
Dan White <dan.white@taxauditsolutions.com>
2025-07-13 19:54:49 +0000

    Now we are back to more text being cut off.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       fontFamily: {
