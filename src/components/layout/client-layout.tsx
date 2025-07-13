58c0259b88fe5574fdf47efc7dca59f5c8ed5b96
Dan White <dan.white@taxauditsolutions.com>
2025-07-13 18:45:33 +0000

    That is very interesting. it took up the entire space and flowed over th

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -29,13 +29,15 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
           </SidebarFooter>
         </Sidebar>
         
-        <main className="relative flex flex-1 flex-col overflow-auto">
-           <header className="sticky top-0 z-10 flex shrink-0 items-center border-b bg-background/80 px-4 py-2 backdrop-blur-sm sm:px-6">
+        <div className="flex flex-1 flex-col overflow-hidden">
+          <header className="sticky top-0 z-10 flex h-[57px] shrink-0 items-center border-b bg-background/80 px-4 py-2 backdrop-blur-sm sm:px-6">
             <SidebarTrigger className="md:hidden" />
-            <div className="flex-1 text-center" />
+            <div className="flex-1" />
           </header>
-          {children}
-        </main>
+          <main className="flex-1 overflow-auto">
+            {children}
+          </main>
+        </div>
 
       </div>
     </SidebarProvider>
