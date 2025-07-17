ff5c2fcdabb5f243a50d90d7dfefee030892b261
Dan White <dan.white@taxauditsolutions.com>
2025-07-17 18:55:32 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,30 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        config.externals.push('@opentelemetry/sdk-node');
+    }
+    return config;
+  }
+};
+
+export default nextConfig;
