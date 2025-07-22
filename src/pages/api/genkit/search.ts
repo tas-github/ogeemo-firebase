319d5235ac5e8b5448140bf0a5e02b4d7db6136e
Dan White <dan.white@taxauditsolutions.com>
2025-07-22 13:26:39 +0000

    Now we are going to think like Dan. When I try to restart my app,    I g

---
+++ src/pages/api/genkit/search.ts
@@ -0,0 +1,26 @@
+
+import { aiSearchFlow } from "../../../genkit";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "POST") {
+        res.status(405).json({ error: "Method Not Allowed" });
+        return;
+    }
+
+    const { query, dataSources } = req.body;
+
+    try {
+        const result = await aiSearchFlow.run({
+            query,
+            dataSources,
+        });
+        res.status(200).json(result);
+    } catch (error: any) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+}
