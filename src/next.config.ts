d1564049214b1eec18b7279e466b858d6b55664d
Dan White <dan.white@taxauditsolutions.com>
2025-07-22 14:39:31 +0000

    Nothing is changing. Is it possible for you to build a test sandbox and

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,24 +1,12 @@
 
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
