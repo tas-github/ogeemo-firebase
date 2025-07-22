a6d35d05e9ffda7239445239068c9a3fd12755a3
Dan White <dan.white@taxauditsolutions.com>
2025-07-22 16:41:57 +0000

    On the header , edit where is says For accountants to "for accountants a

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
     { href: "/for-small-businesses", label: "For Small Businesses" },
-    { href: "#", label: "For Accountants" }, // Placeholder
+    { href: "#", label: "For Accountants and Bookkeepers" }, // Placeholder
     { href: "#", label: "Features" }, // Placeholder
     { href: "#", label: "Beta Program" }, // Placeholder
 ];
