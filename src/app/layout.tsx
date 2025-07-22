01a0dc1a1943dc715266d8304601910f859dd5a1
Dan White <dan.white@taxauditsolutions.com>
2025-07-22 14:31:36 +0000

    I tried all my ways to refresh and what you did not cause anything to ch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,11 @@
 import type { ReactNode } from "react";
-import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
-import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const fontBody = Inter({
-  subsets: ["latin"],
-  variable: "--font-body",
-});
-
-const fontHeadline = SpaceGrotesk({
-  subsets: ["latin"],
-  variable: "--font-headline",
-  weight: "700",
-});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-body antialiased",
-          fontBody.variable,
-          fontHeadline.variable
-        )}
-      >
+    <html lang="en">
+      <body>
         {children}
-        <Toaster />
       </body>
     </html>
   );
