a638e7f5e0ebce8688831ce2b872fd3dedaac013
Dan White <dan.white@taxauditsolutions.com>
2025-07-23 11:31:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/tasks/project-planning-view.tsx
+++ src/components/tasks/project-planning-view.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import React, { useState, useEffect, useCallback } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
