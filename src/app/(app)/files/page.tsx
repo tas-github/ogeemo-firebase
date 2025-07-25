e102ce330c86e14bad30b6cb0e0efef4d30b9686
Dan White <dan.white@taxauditsolutions.com>
2025-07-25 12:50:03 +0000

    Nothing showed up. I am noting that today is worse than normal about thi

--- src/app/(app)/files/page.tsx
+++ src/app/(app)/files/page.tsx
@@ -1,15 +1,1 @@
-
-import dynamic from 'next/dynamic';
-import { FilesSkeleton } from '@/components/files/files-skeleton';
-
-const FilesView = dynamic(
-  () => import('@/components/files/files-view').then((mod) => mod.FilesView),
-  {
-    ssr: false,
-    loading: () => <FilesSkeleton />,
-  }
-);
-
-export default function FilesPage() {
-  return <FilesView />;
-}
+// This file has been cleared as requested.
--- src/components/files/files-view.tsx
+++ src/components/files/files-view.tsx
@@ -1,17 +1,1 @@
-
-"use client";
-
-import React from 'react';
-import { Folder } from 'lucide-react';
-import { FilesViewContent } from './files-view-content';
-
-export function FilesView() {
-  return (
-    <FilesViewContent 
-      rootFolderId={null}
-      headerIcon={Folder}
-      headerTitle="Ogeemo File Manager"
-      headerDescription="A new foundation for managing your files and folders."
-    />
-  );
-}
+// This file has been cleared as requested.
